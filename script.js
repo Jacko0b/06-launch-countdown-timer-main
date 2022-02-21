b2ae32626249c19a0ab4d9f2e0eaaae3e80c57e4
Jacko0b <jakub.sobota.pl@gmail.com>
2022-02-22 00:21:45 +0100

    started script

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-const cards = document.querySelectorAll('.card');
+// const  = document.querySelectorAll('.');
 
-cards.forEach((card)=>{
-    card.addEventListener("click", ()=>{
-        card.classList.toggle("animate");
-        card.innerHTML = "123";
-    });
-});
+// cards.forEach((card)=>{
+//     card.addEventListener("click", ()=>{
+        
+//     });
+// });
+
+let currentDate = new Date();
+let launchDate= new Date("2022-11-07");
+console.log(currentDate.toTimeString);
+// function sleep(miliseconds){
+//     const date = Date.now
+// }
+for (let i=0;i<1000;i++){
+    currentDate= Date.now();
+    console.log(currentDate.toTimeString);
+
+}// while(1){
+//     sleep(1000)
+//     console.log
+// }
