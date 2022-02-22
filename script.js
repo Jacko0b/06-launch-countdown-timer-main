c2af2d1f2f08e326827fbb62d4af09cad414ad62
Jacko0b <jakub.sobota.pl@gmail.com>
2022-02-22 15:25:49 +0100

    Js script modifying

--- script.js
+++ script.js
@@ -1,22 +1,36 @@
-// const  = document.querySelectorAll('.');
 
-// cards.forEach((card)=>{
-//     card.addEventListener("click", ()=>{
-        
-//     });
-// });
+let test= new Date("1-1-1");
+// console.log(test)
+// test.setSeconds(10);
+// test.setMinutes(29);
+// test.setHours(5);
+// test.setDate(20);
+// console.log(test);
 
-let currentDate = new Date();
-let launchDate= new Date("2022-11-07");
-console.log(currentDate.toTimeString);
-// function sleep(miliseconds){
-//     const date = Date.now
-// }
-for (let i=0;i<1000;i++){
-    currentDate= Date.now();
-    console.log(currentDate.toTimeString);
+(function (){
+    const clockDigits = document.querySelectorAll(".clock-digit")
+    const launchDate = new Date("2022-11-07");
 
-}// while(1){
-//     sleep(1000)
-//     console.log
-// }
+    (function updateClock(){
+    const currentDate = new Date();
+    const msLeft = Date.parse(launchDate)-Date.parse(currentDate);
+    let timeLeft = {
+        ms: msLeft,
+        s: Math.round(msLeft/1000 %60),
+        m: Math.round(msLeft/1000 /60 %60),
+        h: Math.round(msLeft/1000 /60 /60 %60),
+        d: Math.round(msLeft/1000 /60 /60 /24 ),
+    };
+    
+    
+    ( function updateAnimation() {
+        clockDigits.forEach(digit => {
+            let flipCard = digit.querySelector(".flip-card")
+            flipCard.classList.toggle("flipped")
+        });
+    })();
+    
+    console.log();
+    setTimeout(updateClock,1000);
+    })();
+})();
